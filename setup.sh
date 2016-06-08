29a2f6e9c958f929014e95dd8f86d1a1ec6f1f3b
y4shiro <y4shironao@gmail.com>
2016-06-08 19:11:44 +0900

    Add ".zshrc"

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_profile …)
+  DOT_FILES=(.bashrc .bash_profile .zshrc  …)
 
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/dotfiles/$file $HOME/$file
-done
+ for file in ${DOT_FILES[@]}
+ do
+     ln -s $HOME/dotfiles/$file $HOME/$file
+ done
