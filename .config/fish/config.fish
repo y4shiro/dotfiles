ded3be7d6b89b2d17c12a001fdf5a247b046a239
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2019-01-07 15:43:29 +0900

    config.fish 内のパス指定を修正

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH "~/.local/share/omf"
+set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG "~/.config/omf"
+set -gx OMF_CONFIG "$HOME/.config/omf"
 
 # homebrew
 set -x PATH /usr/local/bin /usr/local/sbin $PATH
