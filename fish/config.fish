badc59291c4c5f13c0572539886948c63a7d16ee
y4shiro <y4shironao@gmail.com>
2020-09-22 17:23:45 +0900

    rbenv パス修正

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 # rbenv
-set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+set -x PATH $HOME/.rbenv/bin $PATH
+status --is-interactive; and source (rbenv init -|psub)
 
 # homebrew
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
