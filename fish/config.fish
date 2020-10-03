8878a4e5c0b0bbed32b1de0f81085af08dc1c247
y4shiro <y4shironao@gmail.com>
2020-10-03 09:55:55 +0900

    anyenv, nodenv 導入

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,12 @@
+# anyenv
+set -x PATH $HOME/.anyenv/bin $PATH
+status --is-interactive; and source (anyenv init -|psub)
+
+# nodenv
+set -x NDENV_ROOT $HOME/.anyenv/envs/ndenv
+set -x PATH $HOME/.anyenv/envs/ndenv/bin $PATH
+set -x PATH $NDENV_ROOT/shims $PATH
+
 # rbenv
 set -x PATH $HOME/.rbenv/bin $PATH
 status --is-interactive; and source (rbenv init -|psub)
